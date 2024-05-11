bf0000c3505dabe007a3d11bcd3175b0ffaf359b
Bang241002 <bang241002@gmail.com>
2024-05-11 13:40:40 +0700

    1

---
+++ JavaServlet/JavaServlet/src/main/java/com/luongviethoang/filter/Logger.java
@@ -0,0 +1,28 @@
+package com.luongviethoang.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+
+public class Logger implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("filter1");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        servletResponse.setContentType("text/plain");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
